ab95331b42a85700f243ff76f486ab0bdd9aa0de
Mauricio Nunes <mnunes@vialink.com.br>
2017-04-26 14:20:15 -0300

    Adicionado as layers de items e player

---
+++ core/defs/anim.def.php
@@ -0,0 +1,58 @@
+<?php
+
+
+abstract class animDefs {
+{
+	const ANIM_WALK_UNARM							    = 0x00;
+	const ANIM_WALK_ARM									= 0x01;
+	const ANIM_RUN_UNARM								= 0x02;
+	const ANIM_RUN_ARMED								= 0x03;
+	const ANIM_STAND									= 0x04;
+	const ANIM_FIDGET1									= 0x05;
+	const ANIM_FIDGET_YAWN								= 0x06;
+	const ANIM_STAND_WAR_1H								= 0x07;
+	const ANIM_STAND_WAR_2H								= 0x08;
+	const ANIM_ATTACK_1H_WIDE							= 0x09;
+	const ANIM_ATTACK_1H_JAB							= 0x0a;
+	const ANIM_ATTACK_1H_DOWN							= 0x0b;
+	const ANIM_ATTACK_2H_DOWN							= 0x0c;
+	const ANIM_ATTACK_2H_WIDE							= 0x0d;
+	const ANIM_ATTACK_2H_JAB							= 0x0e;
+	const ANIM_WALK_WAR									= 0x0f;
+	const ANIM_CAST_DIR									= 0x10;
+	const ANIM_CAST_AREA								= 0x11;
+	const ANIM_ATTACK_BOW								= 0x12;
+	const ANIM_ATTACK_XBOW								= 0x13;
+	const ANIM_GET_HIT									= 0x14;
+	const ANIM_DIE_BACK									= 0x15;
+	const ANIM_DIE_FORWARD								= 0x16;
+	const ANIM_BLOCK									= 0x1e;
+	const ANIM_ATTACK_UNARM								= 0x1f;
+	const ANIM_BOW										= 0x20;
+	const ANIM_SALUTE									= 0x21;
+	const ANIM_EAT										= 0x22;
+	const ANIM_HORSE_RIDE_SLOW							= 0x17;
+	const ANIM_HORSE_RIDE_FAST							= 0x18;
+	const ANIM_HORSE_STAND								= 0x19;
+	const ANIM_HORSE_ATTACK								= 0x1a;
+	const ANIM_HORSE_ATTACK_BOW							= 0x1b;
+	const ANIM_HORSE_ATTACK_XBOW						= 0x1c;
+	const ANIM_HORSE_SLAP								= 0x1d;
+
+		// These const ANIMations are used by const ANIMals
+	// const ANIMals. (Most All const ANIMals have all const ANIMs)
+	const ANIM_ANI_WALK									= 0x00;
+	const ANIM_ANI_RUN									= 0x01;
+	const ANIM_ANI_STAND								= 0x02;
+	const ANIM_ANI_EAT									= 0x03;
+	const ANIM_ANI_ALERT								= 0x04;
+	const ANIM_ANI_ATTACK1								= 0x05;
+	const ANIM_ANI_ATTACK2								= 0x06;
+	const ANIM_ANI_GETHIT								= 0x07;
+	const ANIM_ANI_DIE1									= 0x08;
+	const ANIM_ANI_FIDGET1								= 0x09;
+	const ANIM_ANI_FIDGET2								= 0x0a;
+	const ANIM_ANI_SLEEP								= 0x0b;
+	const ANIM_ANI_DIE2									= 0x0c;
+}
+?>
---
+++ core/defs/layers.def.php
@@ -0,0 +1,280 @@
+<?php
+/**
+ * Ultima PHP - OpenSource Ultima Online Server written in PHP
+ * Version: 0.1 - Pre Alpha
+ */
+
+abstract class itemLayersDefs {
+	/// <summary>
+		/// Invalid layer.
+		/// </summary>
+		const INVALID	= 0x00;
+		/// <summary>
+		/// First valid layer. Equivalent to <c>Layer.OneHanded</c>.
+		/// </summary>
+		const FIRSTVALID   = 0x01;
+		/// <summary>
+		/// One handed weapon.
+		/// </summary>
+		const ONEHANDED	 = 0x01;
+		/// <summary>
+		/// Two handed weapon or shield.
+		/// </summary>
+		const TWOHANDED	 = 0x02;
+		/// <summary>
+		/// Shoes.
+		/// </summary>
+		const SHOES		 = 0x03;
+		/// <summary>
+		/// Pants.
+		/// </summary>
+		const PANTS		 = 0x04;
+		/// <summary>
+		/// Shirts.
+		/// </summary>
+		const SHIRT		 = 0x05;
+		/// <summary>
+		/// Helmets; hats; and masks.
+		/// </summary>
+		const HELM		 = 0x06;
+		/// <summary>
+		/// Gloves.
+		/// </summary>
+		const GLOVES	= 0x07;
+		/// <summary>
+		/// Rings.
+		/// </summary>
+		const RING		 = 0x08;
+		/// <summary>
+		/// Talismans.
+		/// </summary>
+		const TALISMAN	 = 0x09;
+		/// <summary>
+		/// Gorgets and necklaces.
+		/// </summary>
+		const NECK		 = 0x0A;
+		/// <summary>
+		/// Hair.
+		/// </summary>
+		const HAIR		 = 0x0B;
+		/// <summary>
+		/// Half aprons.
+		/// </summary>
+		const WAIST		 = 0x0C;
+		/// <summary>
+		/// Torso; inner layer.
+		/// </summary>
+		const INNERTORSO	 = 0x0D;
+		/// <summary>
+		/// Bracelets.
+		/// </summary>
+		const BRACELET	 = 0x0E;
+		/// <summary>
+		/// Unused.
+		/// </summary>
+		const UNUSED_XF	 = 0x0F;
+		/// <summary>
+		/// Beards and mustaches.
+		/// </summary>
+		const FACIALHAIR	 = 0x10;
+		/// <summary>
+		/// Torso; outer layer.
+		/// </summary>
+		const MIDDLETORSO	 = 0x11;
+		/// <summary>
+		/// Earings.
+		/// </summary>
+		const EARRINGS	 = 0x12;
+		/// <summary>
+		/// Arms and sleeves.
+		/// </summary>
+		const ARMS		 = 0x13;
+		/// <summary>
+		/// Cloaks.
+		/// </summary>
+		const CLOAK		 = 0x14;
+		/// <summary>
+		/// Backpacks.
+		/// </summary>
+		const BACKPACK	 = 0x15;
+		/// <summary>
+		/// Torso; outer layer.
+		/// </summary>
+		const OUTERTORSO	 = 0x16;
+		/// <summary>
+		/// Leggings; outer layer.
+		/// </summary>
+		const OUTERLEGS	 = 0x17;
+		/// <summary>
+		/// Leggings; inner layer.
+		/// </summary>
+		const INNERLEGS	 = 0x18;
+		/// <summary>
+		/// Last valid non-internal layer. Equivalent to <c>Layer.InnerLegs</c>.
+		/// </summary>
+		const LASTUSERVALID= 0x18;
+		/// <summary>
+		/// Mount item layer.
+		/// </summary>
+		const MOUNT		 = 0x19;
+		/// <summary>
+		/// Vendor 'buy pack' layer.
+		/// </summary>
+		const SHOPBUY		 = 0x1A;
+		/// <summary>
+		/// Vendor 'resale pack' layer.
+		/// </summary>
+		const SHOPRESALE	 = 0x1B;
+		/// <summary>
+		/// Vendor 'sell pack' layer.
+		/// </summary>
+		const SHOPSELL	 = 0x1C;
+		/// <summary>
+		/// Bank box layer.
+		/// </summary>
+		const BANK		 = 0x1D;
+		/// <summary>
+		/// Last valid layer. Equivalent to <c>Layer.Bank</c>.
+		/// </summary>
+		const LASTVALID    = 0x1D;
+    
+}
+
+abstract class playerLayerDef{
+		/// <summary>
+		/// Invalid layer.
+		/// </summary>
+		const Invalid		 = 0x00;
+		/// <summary>
+		/// First valid layer. Equivalent to <c>Layer.OneHanded</c>.
+		/// </summary>
+		const FirstValid   = 0x01;
+		/// <summary>
+		/// One handed weapon.
+		/// </summary>
+		const OneHanded	 = 0x01;
+		/// <summary>
+		/// Two handed weapon or shield.
+		/// </summary>
+		const TwoHanded	 = 0x02;
+		/// <summary>
+		/// Shoes.
+		/// </summary>
+		const Shoes		 = 0x03;
+		/// <summary>
+		/// Pants.
+		/// </summary>
+		const Pants		 = 0x04;
+		/// <summary>
+		/// Shirts.
+		/// </summary>
+		const Shirt		 = 0x05;
+		/// <summary>
+		/// Helmets; hats; and masks.
+		/// </summary>
+		const Helm		 = 0x06;
+		/// <summary>
+		/// Gloves.
+		/// </summary>
+		const Gloves		 = 0x07;
+		/// <summary>
+		/// Rings.
+		/// </summary>
+		const Ring		 = 0x08;
+		/// <summary>
+		/// Talismans.
+		/// </summary>
+		const Talisman	 = 0x09;
+		/// <summary>
+		/// Gorgets and necklaces.
+		/// </summary>
+		const Neck		 = 0x0A;
+		/// <summary>
+		/// Hair.
+		/// </summary>
+		const Hair		 = 0x0B;
+		/// <summary>
+		/// Half aprons.
+		/// </summary>
+		const Waist		 = 0x0C;
+		/// <summary>
+		/// Torso; inner layer.
+		/// </summary>
+		const InnerTorso	 = 0x0D;
+		/// <summary>
+		/// Bracelets.
+		/// </summary>
+		const Bracelet	 = 0x0E;
+		/// <summary>
+		/// Unused.
+		/// </summary>
+		const Unused_xF	 = 0x0F;
+        /// <summary>
+        /// Unused.
+        /// </summary>
+        const Face = 0x0F;
+		/// <summary>
+		/// Beards and mustaches.
+		/// </summary>
+		const FacialHair	 = 0x10;
+		/// <summary>
+		/// Torso; outer layer.
+		/// </summary>
+		const MiddleTorso	 = 0x11;
+		/// <summary>
+		/// Earings.
+		/// </summary>
+		const Earrings	 = 0x12;
+		/// <summary>
+		/// Arms and sleeves.
+		/// </summary>
+		const Arms		 = 0x13;
+		/// <summary>
+		/// Cloaks.
+		/// </summary>
+		const Cloak		 = 0x14;
+		/// <summary>
+		/// Backpacks.
+		/// </summary>
+		const Backpack	 = 0x15;
+		/// <summary>
+		/// Torso; outer layer.
+		/// </summary>
+		const OuterTorso	 = 0x16;
+		/// <summary>
+		/// Leggings; outer layer.
+		/// </summary>
+		const OuterLegs	 = 0x17;
+		/// <summary>
+		/// Leggings; inner layer.
+		/// </summary>
+		const InnerLegs	 = 0x18;
+		/// <summary>
+		/// Last valid non-internal layer. Equivalent to <c>Layer.InnerLegs</c>.
+		/// </summary>
+		const LastUserValid= 0x18;
+		/// <summary>
+		/// Mount item layer.
+		/// </summary>
+		const Mount		 = 0x19;
+		/// <summary>
+		/// Vendor 'buy pack' layer.
+		/// </summary>
+		const ShopBuy		 = 0x1A;
+		/// <summary>
+		/// Vendor 'resale pack' layer.
+		/// </summary>
+		const ShopResale	 = 0x1B;
+		/// <summary>
+		/// Vendor 'sell pack' layer.
+		/// </summary>
+		const ShopSell	 = 0x1C;
+		/// <summary>
+		/// Bank box layer.
+		/// </summary>
+		const Bank		 = 0x1D;
+		/// <summary>
+		/// Last valid layer. Equivalent to <c>Layer.Bank</c>.
+		/// </summary>
+		const LastValid    = 0x1D;
+}
