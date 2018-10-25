64c2218d89032ebca22f6d69e9fee496bf770722
Mauricio Nunes <mutila@gmail.com>
2018-10-25 16:40:25 -0300

    Packet 0xB1 worked and GumpSystem complete!

--- core/packets/0xB1.packet.php
+++ core/packets/0xB1.packet.php
@@ -22,14 +22,55 @@ class packet_0xB1 extends Packets {
             return false;
         }
 
+        $offset = 0;
+
+        $switchesID = array();
+        $textEntries = array();
+
         $command  = $data[0];
-        $packetSize = hexdec(Functions::implodeByte($data, 1, 2));
-        $serial = hexdec(Functions::implodeByte($data, 3, 6));
-        $gumpID = hexdec(Functions::implodeByte($data, 7, 10));
-        $buttonID = hexdec(Functions::implodeByte($data, 11, 14));
-        $switchesCount = hexdec(Functions::implodeByte($data, 15, 18));
-        $dialogQueVeioNoPacote = Map::$gumpsIds[$gumpId];
+        $packetSize = hexdec(Functions::implodeByte($data, 1, $offset+=2));
+        $serial = hexdec(Functions::implodeByte($data, $offset+1, $offset+=4));   
+        $gumpID = hexdec(Functions::implodeByte($data, $offset+1, $offset+=4));
+        $buttonID = hexdec(Functions::implodeByte($data, $offset+1, $offset+=4));
+
+        if($gumpID == hexdec("0x1CD"))
+        {
+            $switchesCount = hexdec(Functions::implodeByte($data, $offset+1, $offset+=4));
+        }
+        else{
+            $switchesCount = hexdec(Functions::implodeByte($data, $offset+1, $offset+=4));
+        }
+
+        for($i=0;$i<$switchesCount;$i++)
+        {
+            $switchesID[] = hexdec(Functions::implodeByte($data, $offset+1, $offset+=4));
+        }
+
+        if($gumpID == hexdec("0x1CD") && $buttonID == hexdec("0x01") && $switchesCount > hexdec("0x00"))
+        {
+            $textEntrieCount = hexdec(Functions::implodeByte($data, $offset+1, $offset+=4));
+        }else{
+            $textEntrieCount = hexdec(Functions::implodeByte($data, $offset+1, $offset+=4));
+        }
+
+        for($i=0;$i<$textEntrieCount;$i++)
+        {
+            $textEntrieID = hexdec(Functions::implodeByte($data, $offset+1, $offset+=2));
+            $height = hexdec(Functions::implodeByte($data, $offset+1, $offset+=2));
+            $textEntry = Functions::readUnicodeStringSafe(str_split(Functions::implodeByte($data, $offset+1, $offset+=$height), 2));
+            $textEntries[] = array(
+                "id" => $textEntrieID,
+                "height" => $height,
+                "textEntry" => $textEntry
+            );
+        }
+
+
+
+
+        $dialogQueVeioNoPacote = Map::$gumpsIds[$gumpID];
         
+        $dialogQueVeioNoPacote::callback($this->client, $buttonID);
 
         return true;
     }
--- tools/UltimaPHPGumpExporter/UltimaPHPExport/UltimaPHPExport.cs
+++ tools/UltimaPHPGumpExporter/UltimaPHPExport/UltimaPHPExport.cs
@@ -108,11 +108,11 @@
                                             {
                                                 if (element4.ButtonType == GumpStudio.Elements.ButtonTypeEnum.Reply)
                                                 {
-                                                    writer.Write("\n\t\t$this->addButton(" + element4.X+", "+ element4.Y + ", "+ element4.NormalID + ", "+ element4.PressedID + ", 1, "+ element4.Param + ", " + element4.Z+");");
+                                                    writer.Write("\n\t\t$this->addButton(" + element4.X+", "+ element4.Y + ", "+ element4.NormalID + ", "+ element4.PressedID +  ", 1, " + element4.Z + ", " + element4.Param + ");");
                                                 }
                                                 else if (element4.ButtonType == GumpStudio.Elements.ButtonTypeEnum.Page)
                                                 {
-                                                    writer.Write("\n\t\t$this->addButton(" + element4.X + ", " + element4.Y + ", " + element4.NormalID + ", " + element4.PressedID + ", 0, " + element4.Param + ", " + element4.Z + ");");
+                                                    writer.Write("\n\t\t$this->addButton(" + element4.X + ", " + element4.Y + ", " + element4.NormalID + ", " + element4.PressedID + ", 0, " + element4.Z + ", " + element4.Param + ");");
                                                 }
                                             }
                                             else
