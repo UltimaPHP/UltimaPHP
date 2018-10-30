569b559bcd9ab78c4910aa18f4b5cce105ec482d
Youri T. K. K. Mattar <youri@youhide.com.br>
2018-10-30 19:28:03 -0300

    fix

--- tools/DockerPush.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ..
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t ultimaphp .
-docker tag ultimaphp hideout/ultimaphp
-docker push hideout/ultimaphp
